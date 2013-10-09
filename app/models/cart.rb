ca3f2417472fe8279c989c13cbcfaeb91390b2b1
mgok <mehmet.gok@gmail.com>
2013-10-09 16:38:48 +0300

    smart cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,6 @@ class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
   def add_product(product_id)
-
     current_item = line_items.find_by(product_id: product_id)
 
     if current_item
@@ -12,8 +11,10 @@ class Cart < ActiveRecord::Base
     end
 
     current_item
-
   end
 
+  def total_price
+    line_items.to_a.sum { |item| item.total_price}
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009051039) do
+ActiveRecord::Schema.define(version: 20131009053022) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
