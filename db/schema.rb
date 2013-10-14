c670a0a065fed3a8c1a234decd6eb40ec8ff9a9c
mgok <mehmet.gok@gmail.com>
2013-10-15 00:55:26 +0300

    rating

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011122053) do
+ActiveRecord::Schema.define(version: 20131014215201) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20131011122053) do
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ratings", force: true do |t|
+    t.string   "name"
+    t.integer  "value"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
